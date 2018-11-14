e0e9d27eb1b7738de4283e724cdad29b233c53a7
nooohooo <woshilizeteng@gmail.com>
2018-11-14 13:24:15 +0800

    解答“三数之和”

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 | 003 | [无重复字符的最长子串](https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/) | [Java](./src/main/java/com/lizeteng/leetcode/medium/_003/Solution.java) | 中等 |
 | 007 | [反转整数](https://leetcode-cn.com/problems/reverse-integer/description/) | [Java](./src/main/java/com/lizeteng/leetcode/easy/_007/Solution.java) | 简单 |
 | 009 | [回文数](https://leetcode-cn.com/problems/palindrome-number/description/) | [Java](./src/main/java/com/lizeteng/leetcode/easy/_009/Solution.java) | 简单 |
+| 015 | [三数之和](https://leetcode-cn.com/problems/3sum/description/) | [Java](./src/main/java/com/lizeteng/leetcode/medium/_015/Solution.java) | 中等 |
 | 019 | [删除链表的倒数第N个节点](https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/) | [Java](./src/main/java/com/lizeteng/leetcode/medium/_019/Solution.java) | 中等 |
 | 020 | [有效的括号](https://leetcode-cn.com/problems/valid-parentheses/description/) | [Java](./src/main/java/com/lizeteng/leetcode/easy/_020/Solution.java) | 简单 |
 | 021 | [合并两个有序链表](https://leetcode-cn.com/problems/merge-two-sorted-lists/description/) | [Java](./src/main/java/com/lizeteng/leetcode/easy/_021/Solution.java) | 简单 |
---
+++ src/main/java/com/lizeteng/leetcode/medium/_015/Solution.java
@@ -0,0 +1,73 @@
+package com.lizeteng.leetcode.medium._015;
+
+import java.util.*;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+ * 使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ *
+ * 示例：
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * 满足要求的三元组集合为：
+ * [
+ *   [-1, 0, 1],
+ *   [-1, -1, 2]
+ * ]
+ *
+ * 注意：
+ * 答案中不可以包含重复的三元组。
+ *
+ * @author lizeteng
+ * @date 2018/11/14
+ */
+public class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        int length = nums.length;
+
+        if (nums == null || length < 3) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1, right = length - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while(left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while(left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
